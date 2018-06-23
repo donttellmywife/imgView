0073d055db04c72104dda05593e7bd1517c528d8
Vladimir Puchkov <volodymyr.puchkov@betlab.com>
2018-06-23 18:51:05 +0300

    feat(index): add event emitter for state

--- src/index.js
+++ src/index.js
@@ -1,2 +1,16 @@
+const EE = require('event-emitter');
+
+const bus = EE();
+const state = {
+  selectedImage: '',
+  selectedRow: '',
+  items: [],
+};
+
 const rootEl = document.querySelector('#root');
-rootEl.innerHTML = 'SOmething funny';
+const update = (value) => {
+  console.log(state);
+  rootEl.innerHTML = value;
+};
+bus.on('update', update);
+bus.emit('update', 'epic');
