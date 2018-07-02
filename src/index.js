0fda43588a68d033cdcdea38a66117d0fd352e2a
Vladimir Puchkov <volodymyr.puchkov@betlab.com>
2018-07-02 20:45:23 +0300

    feat(view): add keyboard controls

--- src/index.js
+++ src/index.js
@@ -1,18 +1,17 @@
 import EE from 'event-emitter';
-import reducer from './reduce.js';
 
 require('./index.sass');
 
+const bus = EE();
+const rootEl = document.querySelector('#root');
 const settings = {
   itemsInRow: 5,
 };
-const state = {
-  x: 3,
-  y: 3,
+let state = {
+  x: 0, // column
+  y: 1, // row
   items: [],
 };
-const ee = EE();
-reducer(ee, state);
 
 const makeEl = (data) => {
   const el = document.createElement('div');
@@ -22,32 +21,74 @@ const makeEl = (data) => {
   return el;
 };
 
-const rootEl = document.querySelector('#root');
+// mock items
+const items = [];
+
 const render = ({ x, y, items }) => {
+  rootEl.innerHTML = '';
   const elements = items.map(makeEl);
   elements
     .map((el, index) => {
       // 0 -> 0, 0
       // 1 -> 1, 0
       // 2 -> 2, 0
-      // 5 -> 5, 0
-      if (x + (y * 5) === index) {
-        console.log(index);
+      // 5 -> 0, 1
+      if (x + (y * settings.itemsInRow) === index) {
         el.classList.add('active');
       }
       return el;
     })
     .forEach((el) => rootEl.append(el));
 };
-ee.on('update', render);
 
-// window.addEventListener('keyup', (event) => {
-//   console.log(event);
-// }, false);
+bus.on('update', (newState) => {
+  state = newState;
+  render(state);
+});
+
+bus.on('keyup', (keyCode) => {
+  switch (keyCode) {
+    // horizontal move (on columns)
+    case 37:
+      if (state.x === 0) { return state; }
+      return bus.emit('update', { ...state, x: state.x - 1 });
+    case 39:
+      // items in row - 1
+      if (state.x === settings.itemsInRow - 1) { return state; }
+      return bus.emit('update', { ...state, x: state.x + 1 });
+
+    // vertical move (on rows)
+    case 38:
+      if (state.y === 0) { return state; }
+      return bus.emit('update', { ...state, y: state.y - 1 });
+    case 40:
+      // load items on prelast row
+      if (state.y === ((state.items.length / settings.itemsInRow) - 3)) {
+        bus.emit('update', { ...state, y: state.y + 1 });
+        return bus.emit('load', state.items.concat(items));
+      }
+
+      // if it's last row - stop
+      if (state.y === (state.items.length / settings.itemsInRow) - 1) {
+        return state;
+      }
+      return bus.emit('update', { ...state, y: state.y + 1 });
+    default:
+      return state;
+  }
+});
+
+bus.on('load', (items) => {
+  bus.emit('update', { ...state, items });
+});
+
+window.addEventListener('keyup', ({ keyCode }) => {
+  bus.emit('keyup', keyCode);
+}, false);
 
 // mock fetch data
-const items = [];
+// const items = [];
 for (let i = 0; i < 50; ++i) {
   items.push(i);
 }
-ee.emit('load', items);
+bus.emit('load', items);
